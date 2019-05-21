462308d01941e780140c431d67a4774f0550b3a2
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2019-05-20 22:23:02 -0400

    toggl bugfix

--- scripts/toggl.sh
+++ scripts/toggl.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 PATH="$PATH":/usr/local/bin
 
-RESULTX="$(/usr/local/bin/toggl now 2>/dev/null; echo x)"
+RESULTX="$(/usr/local/bin/toggl now 2>/dev/null)"
 RESULT="${RESULTX%x}"
-LEN=$(echo ${#RESULTX})
+NLINE=$(printf "$RESULTX" | wc -l)
 
-if [ $LEN -gt 10 ]; then
+if [ $NLINE -gt 1 ]; then
     printf "$RESULT" | head -n 1 | awk -F "#" '{printf $1}'
     printf "| "
-    printf "$RESULT" | awk -F "Duration: " '{print $2}' | grep -v "^$" | sed 's/\(.*\):.*/\1/'
+    printf "$RESULT" | awk -F "Duration: " '{print $2}' | grep -v "^$" # | sed 's/\(.*\):.*/\1/'
 else
     echo No timer is running.
 fi
